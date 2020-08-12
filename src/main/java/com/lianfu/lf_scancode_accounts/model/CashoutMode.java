753247d631633a53155a64961b2a00ba6d06a343
ql <1203229191@qq.com>
2020-08-12 16:33:43 +0800

    第一次

---
+++ src/main/java/com/lianfu/lf_scancode_accounts/model/CashoutMode.java
@@ -0,0 +1,16 @@
+package com.lianfu.lf_scancode_accounts.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CashoutMode {
+   /* private Integer str; 验证git*/
+   private String str;
+
+}
---
+++ src/main/java/com/lianfu/lf_scancode_accounts/web/dlController.java
@@ -0,0 +1,10 @@
+package com.lianfu.lf_scancode_accounts.web;
+
+public class dlController {
+
+
+    public  void  shou(){
+        String sb="你是SB";
+        System.out.println("a");
+    }
+}
